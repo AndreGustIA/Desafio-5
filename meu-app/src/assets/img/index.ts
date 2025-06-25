6c5bbb4d841135790068ac2b4c741c5f9fe49354
Enzo Henry <enzohenry207@gmail.com>
2025-06-25 09:15:14 -0300

    Adiciona imagem de instruções

--- meu-app/src/assets/img/index.ts
+++ meu-app/src/assets/img/index.ts
@@ -3,6 +3,7 @@ import mascote from './mascote.png';
 import googleIcone from './google-icon.png';
 import instagramIcone from './instagram-icon.png';
 import facebookIcone from './facebook-icon.png';
+import nossoApp from './imagem-nossoapp.png';
 
 export const imagens = {
     mascote: mascote,
@@ -10,4 +11,5 @@ export const imagens = {
     iconeGoogle: googleIcone,
     iconeInstagram: instagramIcone,
     iconeFacebook: facebookIcone,
+
 };
--- meu-app/src/pages/NossoApp/NossoApp.tsx
+++ meu-app/src/pages/NossoApp/NossoApp.tsx
@@ -1,7 +1,36 @@
+import './NossoApp.css'; 
+import { imagens } from '../../assets/img';
 const NossoApp = () => {
     return (
-        <h1>Desenvolver Página Conheça o nosso app</h1>
-    )
-}
-
-export default NossoApp;
+        <>
+          <header className="cabecalho">
+            <nav className="navegacao">
+              <ul className="menu">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="instrucoes.html">Instruções</a></li>
+                <li><a href="conhecapp.html">Conheça nosso app</a></li>
+                <li><a href="jogo.html">Jogo</a></li>
+                <li><a href="#">Login</a></li>
+              </ul>
+            </nav>
+          </header>
+    
+          <main className="conteudo-principal">
+            <div className="imagem">
+              <img src={imagens.imagem} alt="Mascote reciclável" className="imagem-nossoapp"/>
+            </div>
+            
+            <h1 className="titulo">INSTRUÇÕES</h1>
+            <p className="descricao">
+              Aprenda brincando! Neste jogo<br />
+              educativo, você vai enfrentar<br />
+              desafios que testam seus <br />
+              conhecimentos sobre o descarte <br />
+              correto do lixo.
+            </p>
+          </main>
+        </>
+     );
+};
+    
+    export default NossoApp;
